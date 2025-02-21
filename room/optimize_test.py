751e34bb091943f8b3c2977ce84535fafa8f8ac7
Clifford William <ccliffordwilliam@gmail.com>
2025-02-22 05:38:12 +0700

    feat: improve bounce login

--- room/optimize_test.py
+++ room/optimize_test.py
@@ -7,7 +7,7 @@ import quadtree_utils
 import random
 
 # Constants
-PARTICLE_COUNT = 1000  # Number of particles
+PARTICLE_COUNT = 200  # Number of particles
 
 # Initialize Pygame
 pygame.init()
@@ -73,16 +73,19 @@ class Particle:
         self.rect.x = x
         self.rect.y = y
 
-        self.max_run: float = 0.09  # Px / ms
+        self.max_run: float = 0.03  # Px / ms
         self.velocity: pygame.Vector2 = pygame.Vector2(0.0, 0.0)
         self.decay: float = 0.01
 
         self.direction_horizontal = 1
         self.direction_vertical = 1
 
+        self.bounce_cooldown = 0  # Cooldown timer in ms
+
     def update(self, dt):
-        # Reset color to default before checking collisions
-        self.surf.fill(self.colr)
+        # Reduce cooldown timer
+        if self.bounce_cooldown > 0:
+            self.bounce_cooldown -= dt
         # Update vel with dir
         self.velocity.x = raycast_utils.exp_decay(self.velocity.x, self.direction_horizontal * self.max_run, self.decay, dt)
         self.velocity.y = raycast_utils.exp_decay(self.velocity.y, self.direction_vertical * self.max_run, self.decay, dt)
@@ -97,6 +100,26 @@ class Particle:
         self.rect.y += self.velocity.y * dt
         # Clamp in screen rect
         self.rect.clamp_ip(room_limit_rect)
+
+    def bounce_with(self, other):
+        """Handles bouncing between two particles"""
+        # Reset color to default before checking collisions
+        if self.bounce_cooldown <= 0 and other.bounce_cooldown <= 0:
+            self.surf.fill("green")
+            # Randomize new directions ensuring they are different
+            new_dirs = [-1, 1]
+            random.shuffle(new_dirs)  # Shuffle to make them unique
+
+            self.direction_horizontal = new_dirs[0]
+            other.direction_horizontal = new_dirs[1]
+
+            random.shuffle(new_dirs)  # Shuffle again for vertical movement
+            self.direction_vertical = new_dirs[0]
+            other.direction_vertical = new_dirs[1]
+
+            # Set cooldown to prevent instant rebouncing
+            self.bounce_cooldown = 300  # 200ms cooldown
+            other.bounce_cooldown = 300
 
     def draw(self, screen: pygame.Surface, camera: pygame.FRect):
         screen.blit(self.surf, (self.rect.x - camera.x, self.rect.y - camera.y))
@@ -126,13 +149,13 @@ while running:
     player.update(keys, dt)
     quad.clear()
     for particle in particles:
+        particle.surf.fill(particle.colr)
         quad.insert(particle)  # you want to put the numerous things that others wanna look for
         # iter per particle to find other particle
         nearby = quad.search(particle.rect) if use_quadtree else [p for p in particles if p.rect.colliderect(particle.rect) and p != particle]
         for other in nearby:
             if other != particle:
-                particle.direction_vertical, other.direction_vertical = other.direction_vertical, particle.direction_vertical
-                particle.direction_horizontal, other.direction_horizontal = other.direction_horizontal, particle.direction_horizontal
+                particle.bounce_with(other)
     
     # use player rect to find nearby particles
     player.surf.fill("red")
---
+++ src/todo.md
@@ -0,0 +1,12 @@
+# references
+
+- (mighty goose)[https://www.youtube.com/watch?v=o_W5MOt1Tgc]
+- (hollow knight)[https://www.youtube.com/watch?v=7-q5qS7TeyI]
+
+1. [ ] create that hollow knight enemy, the ones that fly around idk find out what its called
+2. [ ] dump that enemy into quad tree, each enemy, player, cam and bullets will look for em
+3. [ ] enemy bounces off of walls and each other
+4. [ ] if player touch enemy player gets hurt
+5. [ ] if bullet touch enemy enemy gets hurt
+6. [ ] when player shoot, there should be bullet shell, these ones just interact with tiles but is also in quad for cam to update and draw
+7. [ ] so think about how to use the quad, one quad or multi quad? i guess 1 quad for 1 thing is good, 1 quad for bullet shell and 1 quad for enemy
